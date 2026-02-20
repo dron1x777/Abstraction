3033813efd9093581a3d5752eed13db2f84b94f1
dron1x <usaamidin@gmail.com>
2026-02-20 16:59:23 +0600

    Test project

--- src/Main.java
+++ src/Main.java
@@ -1,105 +1,16 @@
-import java.time.LocalDate;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        while (true) {
-            System.out.print("""
-                    enter region 1-7: 
-                    1 - Osh
-                    2 - Batken
-                    3 - Chyi
-                    4 - Issyk-Kol
-                    5 - Jalal-Abad
-                    6 - Naryn
-                    7 - Talas
-                    : """);
-            int choice = sc.nextInt();
-            switch (choice) {
-                case 1:
-                    System.out.println(Region.OSH.getNameRy());
-                    System.out.println("----------------------------------------------");
-                    System.out.println("""
-                            Древний город Ош
-                           
-                            Гора Сулайман-Тоо
-                            
-                            Часть Ферганской долины
-                            
-                            Развитое сельское хозяйство""");
-                    System.out.println("----------------------------------------------");
-                    break;
-                case 2:
-                    System.out.println(Region.BATKEN.getNameRy());
-                    System.out.println("----------------------------------------------");
-                    System.out.println("""
-                            Самая южная область
-                            
-                            Граничит с Таджикистаном и Узбекистаном
-                            
-                            Сложная приграничная ситуация""");
-                    System.out.println("----------------------------------------------");
-                    break;
-                case 3:
-                    System.out.println(Region.CHYI.getNameRy());
-                    System.out.println("----------------------------------------------");
-                    System.out.println("""
-                            Здесь находится столица — Бишкек
-                            
-                            Самая развитая промышленность
-                            
-                            Плодородная Чуйская долина
-                            
-                            Много заводов и университетов""");
-                    System.out.println("----------------------------------------------");
-                    break;
-                case 4:
-                    System.out.println(Region.ISSYKKOL.getNameRy());
-                    System.out.println("----------------------------------------------");
-                    System.out.println("""
-                            Озеро Иссык-Куль — одно из крупнейших в мире
-                            
-                            Главный туристический регион
-                         
-                            Курорты и санатории""");
-                    System.out.println("---------------------------------------------");
-                    break;
-                case 5:
-                    System.out.println(Region.JALALABAD.getNameRy());
-                    System.out.println("---------------------------------------------");
-                    System.out.println("""
-                            Ореховые леса Арсланбоб
-                            
-                            Озеро Сары-Челек
-                            
-                            Токтогульская ГЭС""");
-                    System.out.println("--------------------------------------------");
-                    break;
-                case 6:
-                    System.out.println(Region.NARYN.getNameRy());
-                    System.out.println("--------------------------------------------");
-                    System.out.println("""
-                            Самая высокогорная область
-                            
-                            Озеро Сон-Куль
-                            
-                            Развито скотоводство""");
-                    System.out.println("---------------------------------------------");
-                    break;
-                case 7:
-                    System.out.println(Region.TALAS.getNameRy());
-                    System.out.println("-------------------------------------------");
-                    System.out.println("""
-                            Родина эпоса Манас
-                            
-                            Меньше население
-                            
-                            Сельское хозяйство""");
-                    System.out.println("---------------------------------------------");
-                    break;
-            }
+        Season[] season = {
+                Season.AUTUMN,
+                Season.WINTER,
+                Season.SUMMER,
+                Season.SPRING
+        };
+        for (Season s : season) {
+            System.out.println(s.getNameRy());
         }
     }
 }
---
+++ src/Season.java
@@ -0,0 +1,17 @@
+public enum Season {
+    SPRING("ВЕСНА"),
+    SUMMER("ЛЕТО"),
+    AUTUMN("ОСЕНЬ"),
+    WINTER("ЗИМА");
+
+    private String nameRy;
+    Season(String nameRy) {
+        this.nameRy = nameRy;
+    }
+    public String getNameRy() {
+        return nameRy;
+    }
+    void setNameRy(String nameRy) {
+        this.nameRy = nameRy;
+    }
+}
