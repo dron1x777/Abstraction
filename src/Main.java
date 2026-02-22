5710f4e02aa43d87dceb7161c2c6da23ed9b0ddd
dron1x <usaamidin@gmail.com>
2026-02-22 11:42:31 +0600

    Test project

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("work days: ");
-        for (Week week: Week.values()) {
-            if(!week.isWeekend()){
-                System.out.println(week);
-                System.out.println("-----------------------------------");
-            }
-        }
-        System.out.println("rest days: ");
-        for(Week week: Week.values()){
-            if(week.isWeekend()){
-                System.out.println(week);
-                System.out.println("--------------------------------");
-            }
+        for (Week week:Week.values()) {
+            System.out.println(week.getDescription());
         }
     }
 }
--- src/Week.java
+++ src/Week.java
@@ -1,19 +1,21 @@
 public enum Week {
-   MONDAY(false),
-    TUESDAY(false),
-    WEDNESDAY(false),
-    THURSDAY(false),
-    FRIDAY(false),
-    SATURDAY(true),
-    SUNDAY(true);
+   MONDAY("Дуйшөмбү куну жава окуйм"),
+    TUESDAY("Шейшемби күнү англис тили сабагын окуйм"),
+    WEDNESDAY("Шаршемби күнү жаны тема отобуз"),
+    THURSDAY("Бейшемби күнү англис тили сабагын окуйм"),
+    FRIDAY("Жума күнү жава окуйм"),
+    SATURDAY("Ишемби күнү өз алдынча практика кылам"),
+    SUNDAY("Жекшемби күнү эс алам");
 
-    private boolean isWeekend;
+    private String description;
 
-    Week(boolean isWeekend) {
-        this.isWeekend = isWeekend;
+    Week(String description) {
+        this.description = description;
     }
-    public boolean isWeekend() {
-        return isWeekend;
+    String getDescription() {
+        return description;
     }
-
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
